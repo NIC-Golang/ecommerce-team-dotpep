52d863e14eb3c16fd836988c9721944d323b1adb
maximusprimeavenger <chuminmaksim@gmail.com>
2025-02-04 00:21:10 +0500

    Setted up driver for PostgreSQL migrations

--- src/core-shop-service/cmd/migrations/migrate.go
+++ src/core-shop-service/cmd/migrations/migrate.go
@@ -1,11 +1,13 @@
 package migrations
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/golang-migrate/migrate/v4/source/github"
 	"github.com/joho/godotenv"
@@ -19,9 +21,24 @@ func RunMigrations() error {
 	}
 	pass, user := os.Getenv("SQL_PASS"), os.Getenv("SQL_USER")
 	postgres_str := fmt.Sprintf("postgres://%s:%s@postgres:5432/project?sslmode=disable", user, pass)
-	m, err := migrate.New(
+	db, err := sql.Open("postgres", postgres_str)
+	if err != nil {
+		return fmt.Errorf("error with connecting to postgres")
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("error occured:%s", err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("error pinging postgres: %v", err)
+	}
+	log.Println("Connected to PostgreSQL successfully")
+	m, err := migrate.NewWithDatabaseInstance(
 		"file://./migrations",
-		postgres_str)
+		"project",
+		driver)
 	if err != nil {
 		return fmt.Errorf("error creating migration object: %v", err)
 	}
